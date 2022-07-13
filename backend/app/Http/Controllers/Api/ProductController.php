f1a1eff0eb65d03b871a05620456118b318494bf
Ulisses Ruiz G <uruiz@philias.mx>
2022-07-13 16:01:56 -0500

    refactor validation rules and move to request dedicated file

--- backend/app/Http/Controllers/Api/ProductController.php
+++ backend/app/Http/Controllers/Api/ProductController.php
@@ -7,21 +7,13 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductResource;
+use App\Http\Requests\StoreProductRequest;
 
 class ProductController extends Controller
 {
-    public function store(Request $request)
+    public function store(StoreProductRequest $request)
 	{
-		$request->validate([
-			'description' => ['required', 'min:3', 'max:150'],
-			'barcode' => ['required', 'alpha_num', 'min:3', 'max:20' , 'unique:products,barcode' , 'unique:products,alternative_code'],
-			'alternative_code' => ['required' , 'alpha_num', 'min:3', 'max:20', 'unique:products,alternative_code', 'unique:products,barcode'],
-			'sale_price' => ['required', 'numeric' ,'min:0.20', 'max:1000000'],
-			'purchase_price' => ['required', 'numeric'],
-			'format_of_sell' => ['required', 'in:pieza,servicio,granel,caja']
-		]);
 		$product = Product::create($request->all());
-
 		return new ProductResource($product);
 	}
 }

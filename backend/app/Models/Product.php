724c93b6ae1e9718a84c873ea74399fea98886c9
Ulisses Ruiz G <uruiz@philias.mx>
2022-08-08 13:51:50 -0500

    [update]: add meilisearch library and custom fields to search

--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -2,12 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Scout\Searchable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, Searchable, SoftDeletes;
 	protected $guarded = [];
+
+	/**
+	 * Get the indexable data array for the model.
+	 */
+	public function toSearchableArray()
+	{
+		$properties = $this->toArray();
+
+		$propertiesToFind = [
+			'id' => $properties['id'],
+			'description' => $properties['description'],
+			'barcode' => $properties['barcode'],
+			'alternative_code' => $properties['alternative_code']
+		];
+
+		return $propertiesToFind;
+	}
 }
